f546782b13444b883a5e40eb5d102c58d33db97f
Sarang Gole <saranggole9106@gmail.com>
2025-01-24 17:09:37 +0530

    Update git.py

--- git.py
+++ git.py
@@ -9,8 +9,14 @@ class DateTimeInfo:
         """Returns the current date in YYYY-MM-DD format."""
         return datetime.now().strftime("%Y-%m-%d")
     
+    def get_day(self):
+        """Returns the current day of the week."""
+        return datetime.now().strftime("%A")
+        
 dt_info = DateTimeInfo()
 print("Time:", dt_info.get_time())
 print("Date:", dt_info.get_date())
+print("Day:", dt_info.get_day())
+
 
 
