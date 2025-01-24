8bcef55cdc95464a6cdb5cdf7a4389df77bc5c21
shweta16 <kadamsweta92@gmail.com>
2025-01-24 16:27:18 +0530

    updated something

--- git.py
+++ git.py
@@ -1,3 +1,8 @@
 from datetime  import datetime
 
 class Time:
+  def get_date(self):
+     """Returns the current date in YYYY-MM-DD format."""
+     return datetime.now().strftime("%Y-%m-%d")
+dt_info = Time()
+print("Date:", dt_info.get_date())
