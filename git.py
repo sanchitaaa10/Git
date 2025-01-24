0e617785705733529be288c5cd2e8258f8acf977
Riddhi Zunjarrao <riddhizunjarrao669@gmail.com>
2025-01-24 17:31:20 +0530

    Update git.py

--- git.py
+++ git.py
@@ -12,11 +12,16 @@ class DateTimeInfo:
     def get_day(self):
         """Returns the current day of the week."""
         return datetime.now().strftime("%A")
-        
+    
+    def get_month(self):
+        """Returns the current month."""
+        return datetime.now().strftime("%B")
+
 dt_info = DateTimeInfo()
 print("Time:", dt_info.get_time())
 print("Date:", dt_info.get_date())
 print("Day:", dt_info.get_day())
+print("Month:", dt_info.get_month())
 
 
 
