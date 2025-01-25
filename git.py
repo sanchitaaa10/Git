15539de48e1befe2cfff5d6266b0439f6c507187
sakshi1013-coder <sakmochi101306@gmail.com>
2025-01-25 10:55:46 +0530

    updated

--- git.py
+++ git.py
@@ -1,3 +1,32 @@
-from datetime  import datetime
+from datetime import datetime
+#code kara ani changes proper kara
 
-class Time:
+class DateTimeInfo:
+    def get_time(self):
+        """Returns the current time in HH:MM:SS format."""
+
+        return datetime.now().strftime("%H:%M:%S")
+    
+    def get_date(self):
+        """Returns the current date in YYYY-MM-DD format."""
+        return datetime.now().strftime("%Y-%m-%d")
+    
+    def get_day(self):
+        """Returns the current day of the week."""
+        return datetime.now().strftime("%A")
+    
+    def get_month(self):
+        """Returns the current month."""
+        return datetime.now().strftime("%B")
+    
+    def get_year(self):
+        """Returns the current year."""
+        return datetime.now().year
+
+dt_info = DateTimeInfo()
+print("Time:", dt_info.get_time())
+print("Date:", dt_info.get_date())
+print("Day:", dt_info.get_day())
+print("Month:", dt_info.get_month())
+print("Year:", dt_info.get_year())
+
